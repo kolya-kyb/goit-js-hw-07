78d690d9227d4f704e2252ecc87dc30ff3efe422
Mykola <107625914+kolya-kyb@users.noreply.github.com>
2022-11-12 17:58:30 +0200

    dz-7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector(".gallery"),
+};
+const getElementGallery = ({ preview, original, description }) =>
+  `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+
+const render = () => {
+  const list = galleryItems.map(getElementGallery);
+  refs.gallery.innerHTML = "";
+  refs.gallery.insertAdjacentHTML("beforeend", list.join(""));
+};
+render();
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250",
+});
